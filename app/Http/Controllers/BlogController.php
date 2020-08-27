c7922d42b1d041adec1dd69aeebc5233c2c95f5e
sapisapi <sapisapi20206@gmail.com>
2020-08-28 05:12:05 +0900

    5

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
 
 
 class BlogController extends Controller
@@ -19,6 +20,7 @@ class BlogController extends Controller
         
         return view('blog.list', compact('blogs',$blogs));
     }
+
     /**
      * ブログ詳細を表示する
      * @param int $id
@@ -35,4 +37,35 @@ class BlogController extends Controller
         
         return view('blog.detail', compact('blog',$blog));
     }
-}
+
+    /**
+     * ブログ登録画面を表示する
+     * @return view
+     */
+    public function showCreate()
+    {
+        return view('blog.form');
+    }
+
+    /**
+     * ブログ登録
+     * @return view
+     */
+    public function exeStore(BlogRequest $request)
+    {
+        //フォームからデータ受け取る
+        $inputs = $request->all();
+
+        \DB::beginTransaction();
+        try{
+            //データ登録
+            Blog::create($inputs);
+            \DB::commit();
+        }catch(\Throwable $e){
+            \DB::rollback();
+            abort(500);
+        }
+
+        \Session::flash('err_msg', 'ブログを登録しました');
+        return redirect(route('blogs'));    }
+    }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,12 @@ use Illuminate\Support\Facades\Route;
 //ブログ一覧画面を表示
 Route::get('/', 'BlogController@showList')->name('blogs');
 
+//ブログ登録画面を表示
+Route::get('/blog/create', 'BlogController@showCreate')->name('create');
+
+//ブログ登録
+Route::post('/blog/store', 'BlogController@exeStore')->name('store');
+
+
 //ブログ詳細画面を表示
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
